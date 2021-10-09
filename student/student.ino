bd11d3719d6a851f8631fd8264699ac572418d80
Vinayak Joshi <vinayak.joshi@sugarboxnetworks.com>
2021-10-09 18:48:25 +0530

    core added feature to connect to WiFi, pull data from API and then display on the piano

--- student/student.ino
+++ student/student.ino
@@ -1,21 +1,46 @@
 
-
+#include "wifi.h"
+#include "webClient.h"
 #include "piano.h"
+#include <Arduino_JSON.h>
 
+
+#define WIFI_SSID "Nitro_net"
+#define WIFI_PASSWORD "bolt9959"
+
+#define GET_KEYS_URL "http://192.168.2.5:1923/keys"
 
 void setup() {
+  Serial.begin(115200);
+  beingWiFi(WIFI_SSID, WIFI_PASSWORD);
   beginPiano();
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  for(unsigned long i=0;i<37;i++){
-    printKeys(1<<i);
-    delay(100);
-    printKeys(1<<i);
-    delay(100);
-    printKeys(1<<i);
-    delay(100);
+  delay(100);
+  if(!wifiConnected()){
+    Serial.println("WiFi not connected");
+    return;
   }
+  WebClient wClient;
+  int code=wClient.Get(GET_KEYS_URL);
+  if( code!=200){
+    Serial.print("HTTP error with code ");
+    Serial.print(code);
+    Serial.print(" the error is : ");
+    Serial.println(wClient.error);
+    return;
+  }
+  
+  JSONVar myObject = JSON.parse(wClient.responseBody.c_str());
+  if(!myObject.hasOwnProperty("keys")){
+    Serial.print("The response was ");
+    Serial.print(wClient.responseBody);
+    Serial.println(" Did not receive keys value");
+    return;
+  }
+  long Keys = (long)myObject["keys"];
+  printKeys(Keys);
 }
