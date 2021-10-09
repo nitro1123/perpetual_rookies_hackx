b17345bbf5af565d0a9536ab50b9d50b290f4fa3
Vinayak Joshi <vinayak.joshi@sugarboxnetworks.com>
2021-10-09 20:52:00 +0530

    core added code to monitor keys and push the same to the server via a post request

--- trainer/trainer.ino
+++ trainer/trainer.ino
@@ -1,17 +1,42 @@
 #include "Pins.h"
+#include "wifi.h"
+#include "webClient.h"
+#include <ArduinoJson.h>
+
+
+#define WIFI_SSID "Nitro_net"
+#define WIFI_PASSWORD "bolt9959"
+
+#define POST_KEYS_URL "http://192.168.2.5:1923/keys"
+
+
+unsigned long nextUpdateTime = millis();
 
 void setup() {
   Serial.begin(115200);
+  beingWiFi(WIFI_SSID, WIFI_PASSWORD);
   initiatePins();
 }
 
-uint64_t prevOutput=0;
-
 void loop() {
+  if(!wifiConnected()){
+    Serial.println("WiFi not connected");
+    delay(5000);
+    return;
+  }
+  if(nextUpdateTime>millis()){
+    return;
+  }
+  nextUpdateTime=millis()+100;
   uint64_t op=getKeys();
-  if(op!=prevOutput){
-    Serial.print("Changed to ");
-    Serial.println(op);
-    prevOutput=op;
+  
+  WebClient wClient;
+  int code=wClient.Post(POST_KEYS_URL, "{\"keys\":"+String(op)+"}");
+  if( code!=200){
+    Serial.print("HTTP error with code ");
+    Serial.print(code);
+    Serial.print(" the error is : ");
+    Serial.println(wClient.error);
+    return;
   }
 }
---
+++ trainer/wifi.h
@@ -0,0 +1,8 @@
+  
+#ifndef WIFI_PIANO_H
+#define WIFI_PIANO_H
+
+void beingWiFi(const char *ssid, const char *password);
+bool wifiConnected();
+
+#endif
